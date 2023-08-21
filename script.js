c442bb6d7168de4712d2b0671a43f82c78ebdd02
kalim shaikh <117559118+KalimGithub@users.noreply.github.com>
2023-08-22 00:13:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 //your JS code here. If required.
 let result = document.getElementById("browser-info");
-result = "You are using " + navigator.userAgent + navigator.appName+ " version " + navigator.appVersion
-console.log(result);
+ans = "You are using " + navigator.userAgent + navigator.appName+ " version " + navigator.appVersion
+result.textContent = ans;
+
+
+// var userAgent = navigator.userAgent;
+// ("User Agent: " + userAgent + navigator.appName + navigator.appVersion);
